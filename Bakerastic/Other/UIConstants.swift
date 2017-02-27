0d27ad15741d3e9e25aac2b4d49e87516e784f50
Karol Stępień <karol.stepien@me.com>
2017-02-27 21:09:59 +0100

    some constants, hex UIColor extension

---
+++ Bakerastic/Other/UIConstants.swift
@@ -0,0 +1,9 @@
+//
+//  UIConstants.swift
+//  Bakerastic
+//
+//  Created by Karol Stępień on 27.02.2017.
+//  Copyright © 2017 carlst. All rights reserved.
+//
+
+import Foundation
---
+++ Bakerastic/UIColor+Bakerastic.swift
@@ -0,0 +1,34 @@
+//
+//  UIColor+Bakerastic.swift
+//  Bakerastic
+//
+//  Created by Karol Stępień on 27.02.2017.
+//  Copyright © 2017 carlst. All rights reserved.
+//
+
+import UIKit
+
+extension UIColor {
+    convenience init(hex: String) {
+        var hexString: String = hex.trimmingCharacters(in: .whitespacesAndNewlines).uppercased()
+        
+        if (hexString.hasPrefix("#")) {
+            hexString.remove(at: hexString.startIndex)
+        }
+        
+        if ((hexString.characters.count) != 6) {
+            self.init(red: 255.0, green: 255.0, blue: 255.0, alpha:1.0)
+        } else {
+            var rgbValue:UInt32 = 0
+            Scanner(string: hexString).scanHexInt32(&rgbValue)
+            
+            self.init(
+                red: CGFloat((rgbValue & 0xFF0000) >> 16) / 255.0,
+                green: CGFloat((rgbValue & 0x00FF00) >> 8) / 255.0,
+                blue: CGFloat(rgbValue & 0x0000FF) / 255.0,
+                alpha: CGFloat(1.0)
+            )
+        }
+
+    }
+}
