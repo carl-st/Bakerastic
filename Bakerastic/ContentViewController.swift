d4fba9a43b07e073e6e9d51c9461d4dc739198d9
Karol Stępień <karol.stepien@me.com>
2017-02-28 12:33:26 +0100

    Additional view for showing images, improvements

--- Bakerastic/ContentViewController.swift
+++ Bakerastic/ContentViewController.swift
@@ -8,19 +8,21 @@
 
 import UIKit
 
-class ViewController: UIViewController, UIGestureRecognizerDelegate  {
+class ContentViewController: UIViewController, UIGestureRecognizerDelegate  {
 
+    @IBOutlet weak var kittenView: UIView!
     var viewModel: ContentViewModel?
+    let kittenViewSize: CGFloat = 80
 
     override func viewDidLoad() {
         super.viewDidLoad()
-        self.view.backgroundColor = Colors.primaryColor
+        self.kittenView.backgroundColor = Colors.primaryColor
         
         ContentServices.sharedInstance.getContent(completion: {_, _ -> Void in})
         
         self.viewModel = ContentViewModel(reload: { () -> Void in
             let halfSizeOfView = 25.0
-            let insetSize = self.view.bounds.insetBy(dx: CGFloat(Int(2 * halfSizeOfView)), dy: CGFloat(Int(2 * halfSizeOfView))).size
+            let insetSize = self.kittenView.bounds.insetBy(dx: CGFloat(Int(2 * halfSizeOfView)), dy: CGFloat(Int(2 * halfSizeOfView))).size
 
             if let viewModel = self.viewModel {
 
@@ -29,16 +31,25 @@ class ViewController: UIViewController, UIGestureRecognizerDelegate  {
                             UInt32(UInt(insetSize.width))))
                     let pointY = CGFloat(UInt(arc4random() %
                             UInt32(UInt(insetSize.height))))
-                    let newView = DraggableView(frame: CGRect(x: 0, y: 0, width: 50, height: 50))
-                    newView.frame = CGRect(x: pointX, y: pointY, width: 50, height: 50)
                     
-                    let imageView = UIImageView(frame: CGRect(x: 0, y: 0, width: 50, height: 50))
-//                    imageView.contentMode = .scaleAspectFit
+                    let newView = DraggableView(frame: CGRect(x: pointX, y: pointY, width: self.kittenViewSize, height: self.kittenViewSize))
+                    
+                    let imageView = UIImageView(frame: CGRect(x: 0, y: 0, width: self.kittenViewSize, height: self.kittenViewSize))
+                    imageView.alpha = 0.0
+                    
                     ContentServices.sharedInstance.getImage(url: URL(string: kitten.imageUrl)!, completion: {
                         success, data in
                         if success {
                             let image = data as! UIImage
                             imageView.image = self.maskImage(image: image, withMask: #imageLiteral(resourceName: "Star"))
+                            imageView.layer.shadowColor = UIColor.black.cgColor
+                            imageView.layer.shadowOpacity = 1
+                            imageView.layer.shadowOffset = CGSize.zero
+                            imageView.layer.shadowRadius = 7
+                            
+                            UIView.animate(withDuration: 1.0, animations: {
+                                imageView.alpha = 1.0
+                            })
                         } else {
                             print(data)
                         }
@@ -48,25 +59,19 @@ class ViewController: UIViewController, UIGestureRecognizerDelegate  {
                     let tapGestureRecognizer = UITapGestureRecognizer(target: self, action: #selector(self.handleTap(recognizer:)))
                     tapGestureRecognizer.delegate = self
                     newView.gestureRecognizers?.append(tapGestureRecognizer)
-                    self.view.addSubview(newView)
+                    self.kittenView.addSubview(newView)
                 }
             }
-
-
-            print("images reloaded")
         }, persistence: PersistenceManager.sharedInstance)
-
-
     }
     
     func handleTap(recognizer: UIGestureRecognizer) {
         let tappedView = recognizer.view as! DraggableView
         if let info = tappedView.content.info {
-            let alert = UIAlertController(title: "Alert", message: "Description: \(info.descriptionText)\n\nTimestamp: \(info.timestamp)", preferredStyle: UIAlertControllerStyle.alert)
+            let alert = UIAlertController(title: "Image details", message: "Description: \(info.descriptionText)\n\nRaw Timestamp: \(info.timestamp)\n\nFormatted Timestamp: \(info.timestamp.string(with: .longTimeAndDate))", preferredStyle: UIAlertControllerStyle.alert)
             alert.addAction(UIAlertAction(title: "OK", style: UIAlertActionStyle.default, handler: nil))
             self.present(alert, animated: true, completion: nil)
         }
-
     }
     
     func maskImage(image: UIImage, withMask maskImage: UIImage) -> UIImage {
